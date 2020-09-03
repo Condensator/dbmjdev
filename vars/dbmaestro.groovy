c0fa46c77d28591e88675367fb1da649f8a09ddc
Oleksandr Kosenko <kosenko.oleks@gmail.com>
2020-09-03 15:45:39 +0300

    fix

--- vars/dbmaestro.groovy
+++ vars/dbmaestro.groovy
@@ -348,7 +348,7 @@ def generateDriftDashboard() {
 		echo "Preparing drift dashboard ${reportFile}"
 		writeFile file: reportFile, text: reportBuffer.toString()
 		def pluginsInstalled = jenkins.model.Jenkins.instance.getPluginManager().getPlugins()
-		if(pluginsInstalled.any(it.getShortName()=="htmlpublisher"))
+		if(pluginsInstalled.any { it.getShortName()=="htmlpublisher"})
 		{
 			publishHTML (target : [allowMissing: false,
  				alwaysLinkToLastBuild: true,
